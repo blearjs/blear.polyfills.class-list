ff6622615bc3dd1e6f7406476120ff2376ed01b6
#云淡然 <cloudcome@163.com>
2016-05-24 14:07:02 +0800

    class-list polyfill

--- src/index.js
+++ src/index.js
@@ -1,30 +1,69 @@
 'use strict';
 
+require('blear.polyfills.string');
+
 if (typeof CLASSICAL !== 'undefined' && CLASSICAL === true) {
     exports.add = function (el, className) {
+        className = String(className).trim();
+
+        if (!className) {
+            return;
+        }
+
         var cur = ' ' + el.className + ' ';
         if (cur.indexOf(' ' + className + ' ') === -1) {
             el.className += ' ' + className;
         }
     };
     exports.remove = function (el, className) {
+        className = String(className).trim();
+
+        if (!className) {
+            return;
+        }
+
         var cur = ' ' + el.className + ' ';
+
         if (cur.indexOf(' ' + className + ' ') > -1) {
             el.className = cur.replace(' ' + className + ' ', '');
         }
     };
     exports.has = function (el, className) {
+        className = String(className).trim();
+
+        if (!className) {
+            return false;
+        }
+
         var cur = ' ' + el.className + ' ';
         return cur.indexOf(' ' + className + ' ') > -1;
     };
 } else {
     exports.add = function (el, className) {
+        className = String(className).trim();
+
+        if (!className) {
+            return;
+        }
+
         el.classList.add(className);
     };
     exports.remove = function (el, className) {
+        className = String(className).trim();
+
+        if (!className) {
+            return;
+        }
+
         el.classList.remove(className);
     };
     exports.has = function (el, className) {
+        className = String(className).trim();
+
+        if (!className) {
+            return false;
+        }
+
         return el.classList.contains(className);
     };
 }
