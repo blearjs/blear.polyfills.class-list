4ad3adf3bf977e78dcbdcab32106f053770d1295
#云淡然 <cloudcome@163.com>
2016-11-26 16:12:39 +0800

    ~ 不再兼容 IE8 及以下

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 'use strict';
 
-require('blear.polyfills.string');
-
 if (typeof CLASSICAL !== 'undefined' && CLASSICAL === true) {
     exports.add = function (el, className) {
         className = String(className).trim();
